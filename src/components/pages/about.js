c6ddbe88303300f307c97f1313da9c83148ad425
jessebettridge <jessebett92@gmail.com>
2019-08-16 01:09:23 -0600

    added content to about / contact

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -2,8 +2,26 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="about-wrapper">
+      <div className="about-grid-wrapper">
+        <div className="dad-image" />
+        <div className="about-grid-description">
+          <p>Diggin in the dirt for yellow rocks...</p>
+        </div>
+        <div className="about-grid-description">
+          <p>Water pump blasts the washplant...</p>
+        </div>
+        <div className="pump-image" />
+
+        <div className="washplant-image" />
+        <div className="about-grid-description">
+          <p>Washplant filters through the tailings...</p>
+        </div>
+        <div className="about-grid-description">
+          <p>There's yer Gold</p>
+        </div>
+        <div className="gold-image" />
+      </div>
     </div>
   );
 };
--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,9 +1,58 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faPhoneSquareAlt } from "@fortawesome/free-solid-svg-icons";
+import { faFacebookSquare } from "@fortawesome/free-solid-svg-icons";
+import { faInstagram } from "@fortawesome/free-solid-svg-icons";
+import { faTwitterSquare } from "@fortawesome/free-solid-svg-icons";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className="contact-wrapper">
+      <div className="main-contact-items">
+        <div className="contact-main">
+          <div className="font-icon">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </div>
+          <div className="contact-detail">
+            <h1>email@example.com</h1>
+          </div>
+        </div>
+        <div className="contact-main">
+          <div className="font-icon">
+            <FontAwesomeIcon icon={faPhoneSquareAlt} />
+          </div>
+          <div className="contact-detail">
+            <h1>555-555-5555</h1>
+          </div>
+        </div>
+      </div>
+      <div className="social-links">
+        <div className="social-link">
+          <div className="social-icon">
+            <FontAwesomeIcon icon={faFacebookSquare} />
+          </div>
+          <div className="social-desc">
+            <p>Like us on Facebook</p>
+          </div>
+        </div>
+        <div className="social-link">
+          <div className="social-icon">
+            <FontAwesomeIcon icon={faInstagram} />
+          </div>
+          <div className="social-desc">
+            <p>Follow us on Instagram</p>
+          </div>
+        </div>
+        <div className="social-link">
+          <div className="social-icon">
+            <FontAwesomeIcon icon={faTwitterSquare} />
+          </div>
+          <div className="social-desc">
+            <p>See us Tweet</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
